324725a949703c0093335e202db956099ce04add
Alex Rocha <alexrochatsi@gmail.com>
2020-01-28 16:22:29 -0300

    Teste
    Teste

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+
+sass.compiler = require('node-sass');
+
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.sass')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch('./sass/**/*.sass', ['sass']);
+});
+
+gulp.task('default', function () {
+
+});
+
